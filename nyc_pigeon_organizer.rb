72492bbf9f15b3b711818b07805b31e7d2ba9577
Daniel Shimield <dxshim90@gmail.com>
2019-02-16 15:27:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,18 @@
 def nyc_pigeon_organizer(data)
-pigeon = {}
-data.each do |first_level, second_level|
-  second_level.each do |third_level, forth_level|
-    forth_level.each do |name|
-      if pigeon[name] == nil
+data.each do |attribute, values|#color, gender, live and there corresponding values
+    values.each do |value, pigeons|#individual values and there corresponding pigeons
+      pigeons.each do |pigeon|#individual pigeons
+        if !(by_name[pigeon])#if the new hash does not contain hash for a particular pigeon, create one
+          by_name[pigeon] = {}
+        end
+        if !(by_name[pigeon][attribute]) #if the hash for each pigeon does contain an array for a particular attribute, create one
+          by_name[pigeon][attribute] = []
+        end
+        by_name[pigeon][attribute] << value.to_s
+      end
+    end
+  end
+  by_name
         
         
     
