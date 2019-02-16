0252be744979f32eb640fc7d11f2e1b977849a2d
Daniel Shimield <dxshim90@gmail.com>
2019-02-16 15:44:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon = {}
+data.each do |first_level, second_level|
+  second_level.each do |third_level, forth_level|
+    forth_level.each do |name|
+     if !(pigeon[name])
+       pigeon[name] = {}
+     end
+     if !(pigeon[name][first_level])
+       pigeon[name][first_level] = []
+     end
+    pigeon[name][first_level] << third_level.to_s
+  end
+end
+end
+pigeon
 end
