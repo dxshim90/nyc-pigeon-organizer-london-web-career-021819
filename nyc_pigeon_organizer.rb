d111dfe28ef38617989449cfec3132d43966015f
Daniel Shimield <dxshim90@gmail.com>
2019-02-16 15:18:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon hash = {}
+data.each do |first_level, second_level|
+  second_level.each do |third_level, forth_level|
+    forth_level.each do |name|
+      if pigeon[name] == nil
+        
+    
 end
